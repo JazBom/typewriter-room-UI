c3d67352c76d96362691ba00466de18363cf581c
Jessica Bommer <xininu@gmail.com>
2021-04-29 14:44:47 +1000

    create basic grid structure for user homepage to display text_items under navigation bar

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -3,12 +3,19 @@ import { Grid } from '@material-ui/core';
 import {Nav} from './Nav';
 import { Register } from './Register';
 import { LogIn } from './LogInOrRegister';
+import { TextCard } from './TextCard';
+
 
 const UserHome = (props) => {
     return (
     <div className="container">
       <Nav/> 
-
+      <Grid container spacing={1}>
+        {/* <Grid className="hamburger list" item xs={3} sm={3} lg={3}>
+          something here
+        </Grid> */}
+        <TextCard/>
+      </Grid> 
     </div>
 )
 };
