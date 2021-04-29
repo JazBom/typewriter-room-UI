ab9f0b75fdd9b8ad795b1330acebd23b4b968cfd
Jessica Bommer <xininu@gmail.com>
2021-04-30 00:06:49 +1000

    minor UI update

--- src/components/AllPages.js
+++ src/components/AllPages.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { TextCard } from "./TextCard";
 
 const AllPages = () => {
-  
+
+
     return(
       <div className="allpages">
-        <h1>All Pages</h1>
+        <h2>All Pages</h2>
         <TextCard/>
       </div>
 )
