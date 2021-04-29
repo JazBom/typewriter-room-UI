847ebf46d9f547ef75bccc51e03bb1131efa7011
Jessica Bommer <xininu@gmail.com>
2021-04-29 14:50:54 +1000

    update AllPages react imports and basic header

--- src/components/AllPages.js
+++ src/components/AllPages.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { TextCard } from "./TextCard";
 
 const AllPages = () => {
-
+  
     return(
       <div className="allpages">
-        <h1>All Pages displayed here</h1>
+        <h1>All Pages</h1>
+        <TextCard/>
       </div>
 )
 };
