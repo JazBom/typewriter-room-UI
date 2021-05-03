6173675cff0c3cfafd04c583ce5c5a7b2b5c103e
Jessica Bommer <xininu@gmail.com>
2021-05-03 11:07:40 +1000

    experiment with breadcrumbs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,18 @@
 import { React, useState } from 'react';
 import { Breadcrumbs, Link } from '@material-ui/core';
+import { DropDownMenu } from './DropDownMenu';
+
 // import { Button, List, ListItem, ListItemText, Collapse, makeStyles, Typography } from '@material-ui/core';
 // import { ExpandLess, ExpandMore } from '@material-ui/icons';
 // import { Route, MemoryRouter } from 'react-router';
 // import PropTypes from 'prop-types';
 
-export const Nav = () => {
+export const Nav = (props) => {
 
 return (
-<>
-<Breadcrumbs aria-label="breadcrumb">
+  // <div><DropDownMenu/></div>
+  
+<Breadcrumbs className="nav">
 <Link color="inherit" href="/home">
     Home
   </Link>
@@ -19,16 +22,15 @@ return (
   <Link color="inherit" href="/allpages/publishedpages">
     PublishedPages
   </Link>
-  <Link color="inherit" href="/allpages/mypages">
+  <Link color="inherit" href="/mypages">
     MyPages
   </Link>
-  <Link color="inherit" href="/allpages/mypages/new">
+  <Link color="inherit" href="/mypages/new">
     New
   </Link>
-  <Link color="inherit" href="/">
+  <Link color="inherit" href="/" onClick={props.logOut}>
     Logout
 </Link>
 </Breadcrumbs>
-</>
 
 )};
