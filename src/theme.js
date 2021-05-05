048d15f59649878bde328834cf971c71eb3c952e
Jessica Bommer <xininu@gmail.com>
2021-05-06 02:29:47 +1000

    change theme colours etc

--- src/theme.js
+++ src/theme.js
@@ -9,13 +9,26 @@ export const theme = createMuiTheme({
   },
   spacing: 2.5,
   palette: {
-        type: 'light',
-        primary: {
-          main: '#7e3fb5',
-        },
-        secondary: {
-          main: '#f50057',
-        },
-      
+    type: 'light',
+    primary: {
+      main: '#00acc1',
+      contrastText: '#1a0802',
     },
+    secondary: {
+      main: '#009688',
+    },
+    background: {
+      default: '#F7F4F3',
+    },
+  },
+  typography: {
+    h3: {
+      fontWeight: 300,
+    },
+    h5: {
+      fontWeight: 300,
+      fontSize: '1.5rem',
+      fontFamily: 'Open Sans',
+    },
+  },
 });
